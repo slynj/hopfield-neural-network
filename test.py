1a44567fd43f5872725fc5c87e3105f738af2367
Lyn <lisa.s.jeong@gmail.com>
2025-02-27 11:26:26 -0500

    refactoring

--- test.py
+++ test.py
@@ -12,26 +12,28 @@ patterns = [
 hnet.train(patterns)
 print("Training Completed.\n")
 
-# 1
-test_pattern = [1, -1, -1, -1, -1]
-print(f"Input Pattern: {test_pattern}\n")
 
-predicted_sync = hnet.predict(test_pattern, mode='sync')
-print(f"Predicted Pattern (sync): {predicted_sync}\n")
+def pattern_gen(n=network_size):
+    return np.random.choice([-1, 1], size=n)
 
-predicted_async = hnet.predict(test_pattern, mode='async')
-print(f"Predicted Pattern (async): {predicted_async}\n")
 
-# 2
-test_pattern = [1, -1, -1, -1, -1]
-print(f"Input Pattern: {test_pattern}\n")
+def sync_testing(test_pattern):
+    print(f"Input Pattern: {test_pattern}\n")
+    
+    predicted_sync = hnet.predict(test_pattern, mode='sync')
+    print(f"Predicted Pattern (sync): {predicted_sync}\n\n")
 
-predicted_sync = hnet.predict(test_pattern, mode='sync')
-print(f"Predicted Pattern (sync): {predicted_sync}\n")
 
-# 3
-test_pattern = [1, 1, -1, -1, -1]
-print(f"Input Pattern: {test_pattern}\n")
+def async_testing(test_pattern):
+    print(f"Input Pattern: {test_pattern}\n")
+    
+    predicted_async = hnet.predict(test_pattern, mode='async')
+    print(f"Predicted Pattern (async): {predicted_async}\n\n")
 
-predicted_sync = hnet.predict(test_pattern, mode='sync')
-print(f"Predicted Pattern (sync): {predicted_sync}\n")
+
+def testing(fn, n):
+    [fn(pattern_gen()) for _ in range(n)]
+
+
+testing(sync_testing, 5)
+testing(async_testing, 5)
