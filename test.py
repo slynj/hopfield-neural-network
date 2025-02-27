4ec956e2415b6e4c8b564b2cdd43ecc5d35a37a8
Lyn <lisa.s.jeong@gmail.com>
2025-02-27 11:05:03 -0500

    more tests, now correct

--- test.py
+++ test.py
@@ -12,11 +12,26 @@ patterns = [
 hnet.train(patterns)
 print("Training Completed.\n")
 
+# 1
 test_pattern = [1, -1, -1, -1, -1]
-print(f"Input Pattern: {test_pattern}")
+print(f"Input Pattern: {test_pattern}\n")
 
 predicted_sync = hnet.predict(test_pattern, mode='sync')
-print(f"Predicted Pattern (sync): {predicted_sync}")
+print(f"Predicted Pattern (sync): {predicted_sync}\n")
 
 predicted_async = hnet.predict(test_pattern, mode='async')
-print(f"Predicted Pattern (async): {predicted_async}")
+print(f"Predicted Pattern (async): {predicted_async}\n")
+
+# 2
+test_pattern = [1, -1, -1, -1, -1]
+print(f"Input Pattern: {test_pattern}\n")
+
+predicted_sync = hnet.predict(test_pattern, mode='sync')
+print(f"Predicted Pattern (sync): {predicted_sync}\n")
+
+# 3
+test_pattern = [1, 1, -1, -1, -1]
+print(f"Input Pattern: {test_pattern}\n")
+
+predicted_sync = hnet.predict(test_pattern, mode='sync')
+print(f"Predicted Pattern (sync): {predicted_sync}\n")
