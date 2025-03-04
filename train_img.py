b40d722b7c11f73417db0a8ba1211017cfc4ce67
Lyn <lisa.s.jeong@gmail.com>
2025-03-04 14:32:06 -0500

    added img_corrupted() fn

--- train_img.py
+++ train_img.py
@@ -20,15 +20,37 @@ def img_preprocessing(path, w=128, h=128):
     Returns:
         numpy.ndarray: 1D array with the length w * h. 
     """
+    # convert img
     img = skimage.io.imread(path)
     img_bw = skimage.color.rgb2gray(img)
     img_resize = skimage.transform.resize(img_bw, (w, h), mode='reflect')
 
+    # set state
     threshold = skimage.filters.threshold_mean(img_resize)
     binary = img_resize > threshold
     bipolar = 2 * (binary * 1) - 1
 
+    # resize to 1D array
     flatten = np.reshape(bipolar, (w * h))
 
     return flatten
 
+
+def img_corrupted(input, level):
+    """ Generate corrupted input given probability and input array (1D array)
+
+    Args:
+        input (array): 1D array
+        level (flaot): Probability of each value being inverted
+
+    Returns:
+        array: 1D array, corrupted
+    """
+    corrupted = np.copy(input)
+    invert = np.random.binomial(n=1, p=level, size=len(input))
+
+    for i, v in enumerate(input):
+        if invert[i]:
+            corrupted[i] = -1 * v
+
+    return corrupted
